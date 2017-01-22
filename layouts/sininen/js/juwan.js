608f1fe3aa2e3a8f8fb0b58f37cd00e156a9f8ed
JuwanPark <park_juwan@naver.com>
2017-01-22 10:21:19 +0900

    Add files via upload

--- layouts/sininen/js/juwan.js
+++ layouts/sininen/js/juwan.js
@@ -2,6 +2,12 @@ jQuery(function($){
 	function wresize() {
 		var bodyheight = $(window).height() - ($('#siteheader').outerHeight() + $('#sitefooter').outerHeight());
 		$('#sitebody').css('min-height', bodyheight + 'px');
+		
+		if ($(window).width() > 1000) {
+			$('#toTop').css('right', (($(window).width() - 1000) / 2) + 'px');
+		} else {
+			$('#toTop').css('right', 0);
+		}
 	}
 
 	$(document).ready(function() {
@@ -16,9 +22,24 @@ jQuery(function($){
 			$('#login_bg').fadeOut(250);
 			return false;
 		});
+		
+		$("#toTop").click(function () {
+			$("body").animate({ scrollTop: 0 }, 200);
+			return false;
+		});
 	});
 
 	$(window).resize(function() {
 		wresize();
 	});
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() != 0) {
+			/* $('#toTop').fadeIn(); */
+			$('#toTop').fadeIn(200);
+		} else {
+			/* $('#toTop').fadeOut(); */
+			$('#toTop').fadeOut(200);
+		}
+	});	
 });
